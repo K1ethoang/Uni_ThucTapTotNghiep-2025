e2ce6d8ff86ad27c6774522665cddec0cc69b597
Kiet Hoang Gia <kiethoang101@gmail.com>
2025-03-03 15:29:52 +0700

    feat: admin page

--- applications/employee/admin.py
+++ applications/employee/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
+from applications.employee.models import Employee, Department, DeptManager, Title, Salary, DeptEmp
+
 # Register your models here.
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('emp_no', 'first_name', 'last_name', 'gender',)
+    search_fields = ('emp_no', 'first_name', 'last_name', 'gender',)
+    list_filter = ('gender',)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('dept_no', 'dept_name', )
+    search_fields = ('dept_name',)
+
+
+@admin.register(DeptManager)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('emp_no', 'dept_no',)
+    search_fields = ('emp_no',)
+
+
+@admin.register(Title)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'emp_no', 'from_date', 'to_date',)
+    search_fields = ('dept_name',)
+
+
+@admin.register(DeptEmp)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('emp_no', 'dept_no',)
+    search_fields = ('emp_no',)
+    list_filter = ('dept_no',)
+
+
+@admin.register(Salary)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('emp_no', 'salary', 'from_date', 'to_date',)
+    search_fields = ('emp_no',)
--- applications/employee/models.py
+++ applications/employee/models.py
@@ -1,83 +1,89 @@
 from django.db import models
 
 
-class Employee(models.Model):
-    emp_no = models.AutoField(primary_key=True)
-    birth_date = models.DateField()
-    first_name = models.CharField(max_length=14)
-    last_name = models.CharField(max_length=16)
-    gender = models.CharField(max_length=1, choices=[("M", "Male"), ("F", "Female")])
-    hire_date = models.DateField()
-
-    class Meta:
-        db_table = "employees"
-
-    def __str__(self):
-        return f"{self.emp_no} - {self.first_name} {self.last_name}"
-
-
 class Department(models.Model):
     dept_no = models.CharField(primary_key=True, max_length=4)
     dept_name = models.CharField(unique=True, max_length=40)
 
     class Meta:
-        db_table = "departments"
+        managed = False
+        db_table = 'departments'
 
     def __str__(self):
-        return self.dept_name
+        return f'{self.dept_name}'
 
 
 class DeptEmp(models.Model):
-    emp_no = models.ForeignKey(Employee, on_delete=models.CASCADE, db_column="emp_no")
-    dept_no = models.ForeignKey(Department, on_delete=models.CASCADE, db_column="dept_no")
+    emp_no = models.OneToOneField('Employee', models.DO_NOTHING, db_column='emp_no', primary_key=True)  # The composite primary key (emp_no, dept_no) found, that is not supported. The first column is selected.
+    dept_no = models.ForeignKey(Department, models.DO_NOTHING, db_column='dept_no')
     from_date = models.DateField()
     to_date = models.DateField()
 
     class Meta:
-        db_table = "dept_emp"
-        unique_together = (("emp_no", "dept_no"),)
+        managed = False
+        db_table = 'dept_emp'
+        unique_together = (('emp_no', 'dept_no'),)
 
     def __str__(self):
-        return f"{self.emp_no} - {self.dept_no}"
+        return f'{self.emp_no} - {self.dept_no}'
 
 
 class DeptManager(models.Model):
-    emp_no = models.ForeignKey(Employee, on_delete=models.CASCADE, db_column="emp_no")
-    dept_no = models.ForeignKey(Department, on_delete=models.CASCADE, db_column="dept_no")
+    emp_no = models.OneToOneField('Employee', models.DO_NOTHING, db_column='emp_no', primary_key=True)  # The composite primary key (emp_no, dept_no) found, that is not supported. The first column is selected.
+    dept_no = models.ForeignKey(Department, models.DO_NOTHING, db_column='dept_no')
     from_date = models.DateField()
     to_date = models.DateField()
 
     class Meta:
-        db_table = "dept_manager"
-        unique_together = (("emp_no", "dept_no"),)
+        managed = False
+        db_table = 'dept_manager'
+        unique_together = (('emp_no', 'dept_no'),)
 
     def __str__(self):
-        return f"Manager {self.emp_no} - {self.dept_no}"
+        return f'{self.emp_no} - {self.dept_no}'
+
+
+class Employee(models.Model):
+    emp_no = models.IntegerField(primary_key=True)
+    birth_date = models.DateField()
+    first_name = models.CharField(max_length=14)
+    last_name = models.CharField(max_length=16)
+    gender = models.CharField(max_length=1, choices=[('M', 'Male'), ('F', 'Female')])
+    hire_date = models.DateField()
+
+    class Meta:
+        managed = False
+        db_table = 'employees'
+
+    def __str__(self):
+        return f'{self.emp_no} - {self.first_name} {self.last_name}'
 
 
 class Salary(models.Model):
-    emp_no = models.ForeignKey(Employee, on_delete=models.CASCADE, db_column="emp_no")
+    emp_no = models.OneToOneField(Employee, models.DO_NOTHING, db_column='emp_no', primary_key=True)  # The composite primary key (emp_no, from_date) found, that is not supported. The first column is selected.
     salary = models.IntegerField()
     from_date = models.DateField()
     to_date = models.DateField()
 
     class Meta:
-        db_table = "salaries"
-        unique_together = (("emp_no", "from_date"),)
+        managed = False
+        db_table = 'salaries'
+        unique_together = (('emp_no', 'from_date'),)
 
     def __str__(self):
-        return f"{self.emp_no} - {self.salary}"
+        return f'{self.emp_no} - from {self.from_date} to {self.to_date}'
 
 
 class Title(models.Model):
-    emp_no = models.ForeignKey(Employee, on_delete=models.CASCADE, db_column="emp_no")
+    emp_no = models.OneToOneField(Employee, models.DO_NOTHING, db_column='emp_no', primary_key=True)  # The composite primary key (emp_no, title, from_date) found, that is not supported. The first column is selected.
     title = models.CharField(max_length=50)
     from_date = models.DateField()
     to_date = models.DateField(blank=True, null=True)
 
     class Meta:
-        db_table = "titles"
-        unique_together = (("emp_no", "title", "from_date"),)
+        managed = False
+        db_table = 'titles'
+        unique_together = (('emp_no', 'title', 'from_date'),)
 
     def __str__(self):
-        return f"{self.emp_no} - {self.title}"
+        return f'{self.emp_no} - {self.title}'
