de698dc85f96e32d49a36b79151f7062f4653978
Kiet Hoang Gia <kiethoang101@gmail.com>
2025-03-05 21:04:49 +0700

    feat: set up service

---
+++ docker/register-connector.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Kiểm tra xem Kafka Connect đã sẵn sàng chưa
+while ! curl -s http://kafka-connect:8083/connectors; do
+  echo "Kafka Connect is not ready yet. Retrying in 5 seconds..."
+  sleep 5
+done
+
+echo "Registering MySQL Debezium Connector..."
+
+envsubst < /register-mysql.json | curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" \
+  http://kafka-connect:8083/connectors/ -d @-
+
+echo "Connector registered successfully!"
---
+++ test_db/load-data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e  # Dừng nếu có lỗi
+
+echo "Waiting for MySQL to start..."
+until mysql -h mysql -u root -p"${DB_MYSQL_PASSWORD}" -e "SELECT 1;" &> /dev/null; do
+  echo "MySQL is not ready yet. Waiting..."
+  sleep 10
+done
+
+echo "MySQL is ready!"
+
+echo "Go to test_db folder"
+
+cd /test_db
+
+echo "Running database initialization..."
+mysql -h mysql -u root -p"${DB_MYSQL_PASSWORD}" "${DB_MYSQL_DATABASE}" < employees.sql
+
+echo "Database initialization completed!"
